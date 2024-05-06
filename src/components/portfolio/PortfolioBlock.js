0df263488d346b9abaadcc8da621f3a89fc11b9f
ijotprabh <52730675+ijotprabh@users.noreply.github.com>
2024-05-06 12:52:27 +0530

    Update PortfolioBlock.js

--- src/components/portfolio/PortfolioBlock.js
+++ src/components/portfolio/PortfolioBlock.js
@@ -7,7 +7,7 @@ function PortfolioBlock(props) {
    return (
       <Box display={'flex'} flexDirection={'column'} justifyContent={'center'} alignItems={'center'}>
          <Box component={'img'} src={image} alt={'mockup'}/>
-         <h1 style={{fontSize: '2rem'}}>{title}</h1>
+         <h1 style={{fontSize: '2rem',margin:'15px'}}>{title}</h1>
          <Box className={'portfolio'} display={'flex'} flexDirection={'column'} gap={'0.5rem'}
               alignItems={'center'} fontSize={'1.5rem'} py={'2rem'}>
             <Box p={1} border={'2px solid black'} borderRadius={'25px'}>
